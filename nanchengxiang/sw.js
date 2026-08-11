fabf7e05558ee1655e332bb36386935eee81f492
zhoushunkai <575962901@qq.com>
2026-08-11 18:22:06 +0800

    fix: add missing comma in seedData offlineRecords; bump sw to v43

--- nanchengxiang/sw.js
+++ nanchengxiang/sw.js
@@ -1,34 +1,68 @@
-const CACHE_NAME = 'nanchengxiang-pwa-v42';
+const CACHE_NAME = 'nanchengxiang-pwa-v43';
+
 const ASSETS = [
+
   './',
+
   './index.html',
+
   './manifest.json',
+
   './apple-touch-icon.png',
+
   './icon-192.png',
+
   './icon-512.png',
+
   './css/app.css',
+
   './js/app.js',
+
   './js/pages.js'
+
 ];
 
+
+
 self.addEventListener('install', e => {
+
   e.waitUntil(
+
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+
   );
+
   self.skipWaiting();
+
 });
+
+
 
 self.addEventListener('activate', e => {
+
   e.waitUntil(
+
     caches.keys().then(keys => Promise.all(
+
       keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+
     ))
+
   );
+
   self.clients.claim();
+
 });
 
+
+
 self.addEventListener('fetch', e => {
+
   e.respondWith(
+
     caches.match(e.request).then(r => r || fetch(e.request))
+
   );
+
 });
+
